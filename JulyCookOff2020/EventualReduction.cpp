ecae41f0517d9267632921626c2bc15d4768f3a1
Rishabh Kukreti <rishkuko@gmail.com>
2020-08-16 22:02:22 +0530

    Add files via upload

---
+++ JulyCookOff2020/EventualReduction.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main() {
+  short int t;
+  cin>>t;
+  while(t--) {
+    int n;
+    cin>>n;
+    char arr[n];
+    for(int i=0; i<n; i++) cin>>arr[i];
+
+    for(int i=0; i<n; i++){
+      for(int j=i; j<n; j++){
+        if(arr[i]==arr[j]){
+          char start=i, end=j;
+          for(int k=start; k<=end; k++){
+            for(int a=; a<; a++){
+          }
+        }
+      }
+    }
+
+    bool continue=true;
+    do{
+      for(int i=0; i<n-1; i++){
+        if(arr[i]==arr[i+1]){
+          for(int i=0; i<n-3; i++) arr[i]=arr[i+2];
+          continue=true;
+          break;
+        }
+      }
+    }while(continue==true);
+  }
+  return 0;
+}
